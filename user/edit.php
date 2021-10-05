a0c16e57080bc0643c344dcc4efa9a340605b141
Frodenils <71440881+Frodenils@users.noreply.github.com>
2021-10-05 11:14:31 +0200

    Add files via upload

--- user/edit.php
+++ user/edit.php
@@ -1,103 +1,130 @@
 <?php
-//On demarre une sesssion
+// On démarre une session
 session_start();
 
-//Esct ce que la variable global $_POST['label']n'est pas vide
-if ((isset($_POST['libelle'])) && (!empty($_POST['libelle']))) {
-    //
-    $id = strip_tags($_POST['id_t']);
-    $label = strip_tags($_POST['libelle']);
+// On inclut le Manager
+require_once('Manager/UsersManager.php');
 
-    // On se connecte à la base de données
-    include_once 'connect.php';
-    //On exécute la reque^te SQL et on stocke le résultat dans un tableau associatif
+// On inclut la classe User
+require_once('Entity/User.php');
 
-    $sql = 'UPDATE type_livre SET libelle = ? WHERE id_t = ?;';
+// On inclut le fichier de configuration
+require_once("../conf.php");
 
-    //on prepare la requete
-    $stmt = mysqli_prepare($db, $sql);
+try {
+    $db = new PDO($dsn, $user, $password);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); // Si toutes les colonnes sont converties en string
 
-    // on relie la variable libelle et id
-    mysqli_stmt_bind_param($stmt, 'si', $label, $id);
+    // Créer une instance de la classe UsersManager (un objet $manager)
+    $manager = new UsersManager($db);
 
-//on execute la requete
-    mysqli_stmt_execute($stmt);
+    if ($_POST) {
+        if (
+            isset($_POST['id']) && !empty($_POST['id'])
+            && isset($_POST['email']) && !empty($_POST['email'])
+        ) {
 
-    mysqli_stmt_close($stmt);
-    //On ferme la connexion
-    include_once 'close.php';
+            // On nettoie les données envoyées
+            $id = strip_tags($_POST['id']);
+            $email = strip_tags($_POST['email']);
 
-    $_SESSION['message'] = 'Le type de livre "' . $label . '"a été modifié';
+            $user = $manager->getOne($id);
 
-    header('Location: index.php');
+            // On vérifie si le type existe
+            if (!$user) {
+                // On rédige le message d'erreur qui sera affiché à l'utilisateur
+                $_SESSION['error'] = "Cet id n'existe pas";
 
-} else {
-//Esct ce que l'id existe et n'est pas vite dans l'URL
-    if ((isset($_GET['id_t'])) && !empty($_GET['id_t'])) {
-        $id = strip_tags($_GET['id_t']);
+                // On renvoie vers la page principale
+                header('Location: index.php');
+                exit();
+            }
 
-        // On se connecte à la base de données
-        include_once 'connect.php';
-        //On exécute la reque^te SQL et on stocke le résultat dans un tableau associatif
+            // Modifier le type
+            $user->setEmail($email);
+            $manager->update($user);
 
-        $sql = 'SELECT id_t, libelle FROM type_livre WHERE id_t = ?;';
+            // On rédige le message qui sera affiché à l'utilisateur
+            $_SESSION['message'] = "Utilisateur  modifié";
 
-        //on prepare la requete
-        $stmt = mysqli_prepare($db, $sql);
+            // On renvoie vers la page principale
+            header('Location: index.php');
+        } else {
+            // On rédige le message d'erreur qui sera affiché à l'utilisateur
+            $_SESSION['error'] = "Le formulaire est incomplet";
 
-        // on relie la variable id
-        mysqli_stmt_bind_param($stmt, 'i', $id);
-
-//on execute la requete
-        mysqli_stmt_execute($stmt);
-
-        //on definit les variable qui va recup le type de livre
-        mysqli_stmt_bind_result($stmt, $id, $label);
-
-        mysqli_stmt_fetch($stmt);
-
-        //On ferme la connexion
-        include_once 'close.php';
-
+            // On renvoie vers la page principale
+            header('Location: index.php');
+        }
     }
-}
 
+    // Est-ce que l'id existe et n'est pas vide dans l'URL
+    if (isset($_GET['id']) && !empty($_GET['id'])) {
+
+        // On nettoie l'id envoyé
+        $id = strip_tags($_GET['id']);
+
+        $user = $manager->getOne($id);
+
+        // On vérifie si le type existe
+        if (!$user) {
+            // On rédige le message d'erreur qui sera affiché à l'utilisateur
+            $_SESSION['error'] = "Cet id n'existe pas";
+
+            // On renvoie vers la page principale
+            header('Location: index.php');
+            exit();
+        }
+    } else {
+        // On rédige le message d'erreur qui sera affiché à l'utilisateur
+        $_SESSION['error'] = "URL invalide";
+
+        // On renvoie vers la page principale
+        header('Location: index.php');
+        exit();
+    }
+} catch (PDOException $e) {
+    print('<br/>Erreur de connexion : ' . $e->getMessage());
+}
 ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-        <title>Oui</title>
-    </head>
-    <body>
-    <main class ="container">
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modifier un utilisateur</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+
+<body>
+    <main class="container">
         <div class="row">
             <section class="col-12">
-            <h1>Mofier le type de  livre <?php print($label);?></h1>
-
-            <form method="POST">
-            <div class="mb-3">
-
-            <p>ID : <?php print($id);?> </p>
-
-
-            <label for="label">Libéllé</label>
-            <input type="hidden" id='id_t' name="id_t" value="<?php print($id);?>">
-            <input type="text" id="libelle" name="libelle" class="form-control" value="<?php print($label);?>">
-            </div>
-            <p>
-
-                <a class ="btn btn-info" href="index.php"> Retour à la liste </a>
-                <button class="btn btn-primary"> Modifier</button>
-
-            </p>
-            </form>
+                <?php
+                if (!empty($_SESSION['error'])) {
+                    echo '<div class="alert alert-danger" role="alert">
+                                ' . $_SESSION['error'] . '
+                            </div>';
+                    $_SESSION['error'] = "";
+                }
+                ?>
+                <h1>Modifier un utilisateur</h1>
+                <form method="post">
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="text" id="email" name="email" class="form-control" value="<?= $user->getEmail() ?>">
+                        <label for="email">Rôles</label>
+                        <input type="text" id="roles" name="roles" class="form-control" value="<?= $user->getRoles() ?>">
+                    </div>
+                    <input type="hidden" value="<?= $user->getId() ?>" name="id">
+                    <button class="btn btn-primary">Enregistrer</button>
+                </form>
             </section>
         </div>
     </main>
+</body>
 
-    </body>
-    </html>
+</html>
